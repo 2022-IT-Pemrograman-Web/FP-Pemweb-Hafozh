5a40c60af6af663c16234f21bd3fb6bb4d1ea58d
wahyurizaldi <wahyurizaldi80@gmail.com>
2022-12-10 00:27:09 +0700

    fp yak ges

---
+++ backend/index.js
@@ -0,0 +1,193 @@
+import { db, auth } from './config/firebase.js';
+import admin from './config/firebase-service-account.config.js';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+app.get('/hello', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.post('/', (req, res) => {
+    const { email, password } = req.body;
+    
+    try {
+        signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            return user
+        })
+        .then((user) => {
+            admin.auth().createSessionCookie(user.accessToken, { expiresIn: 60 * 60 * 24 * 5 * 1000 })
+            .then((sessionCookie) => {
+                const options = { maxAge: 60 * 60 * 24 * 5 * 1000, httpOnly: true, secure: true };
+                return res.send({
+                    code: 200,
+                    message: "Login successfull",
+                    result: {
+                        logged_in: true,
+                        session: sessionCookie,
+                        options
+                    }
+                })
+            })
+        })
+        .catch((error) => {
+            res.status(400).json({ message: error.message });
+        });
+    } catch (error) {
+        return res.status(500).send({
+            code: 500,
+            message: 'Internal Server Error',
+            error
+        })
+    }
+});
+
+
+app.post('/register', (req, res) => {
+    const { email, password, cpassword } = req.body;
+
+    try {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userRecord) => {
+            res.send({
+                code: 200,
+                message: "User registered successfully",
+                result: {
+                    userRecord
+                }
+            })
+        })
+        .catch((error) => {
+            res.status(400).json({ message: error.message });
+        });
+    } catch (error) {
+        return res.status(500).send({
+            code: 500,
+            message: 'Internal Server Error',
+            error
+        })
+    }
+});
+
+app.post('/signout', (req, res) => {
+    admin.auth().revokeRefreshTokens(req.user.sub)
+    .then(() => {
+        return admin.auth().getUser(req.user.user_id)
+    })
+    .then((userRecord) => {
+        return res.send({
+            code: 200,
+            message: 'User record fetched successfully',
+            result: {
+                userRecord
+            }
+        })
+    })
+    .catch((error) => {
+        return res.status(500).send({
+            code: 500,
+            message: 'Internal Server Error',
+            error
+        })
+    });
+})
+
+app.get("/dashboard", (req, res) => {
+    try {
+      db.collection("urls")
+        .get()
+        .then((querySnapshot) => {
+          let linky = [];
+          let id;
+          querySnapshot.forEach((doc) => {
+            id = doc.id;
+            linky.push({ id, ...doc.data() });
+          });
+          res.send(linky);
+        });
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
+  app.post("/dashboard", (req, res) => {
+    const { alias, url, code } = req.body;
+    try {
+        db.collection("urls")
+      .add({
+        alias: alias,
+        url: url,
+        code: code,
+        editState: false,
+      })
+    
+      res.send( alias, url, code);
+      console.log(req.body)
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
+  app.delete('/linky/:id', (req, res) => {
+    try {
+        db.collection("urls")
+        .doc(req.params.id).delete()
+        .then(() => {
+            return res.send({
+                code: 200,
+                message: 'Link deleted successfully',
+            });
+        })
+        .catch((error) => {
+            return res.status(500).send({
+                code: 500,
+                message: 'Internal Server Error: Failed to update database',
+                error
+            })
+        });
+    } catch (error) {
+        return res.status(500).send({
+            code: 500,
+            message: 'Internal Server Error',
+            error
+        })
+    }
+});
+
+// app.post('/linky/:id', (req, res) => {
+//     const { alias, url, code } = req.body;
+//     try {
+//       db.collection("urls")
+//         .doc(req.params.id)
+//         .update({
+//           alias: alias,
+//           url: url,
+//           code: code,
+//           editState: false,
+//         })
+//         .then(() => {
+//             res.send( alias, url, code);
+//       console.log(req.body)
+//         })
+//     } catch (error) {
+//       res.send({
+//         status: false,
+//         message: "Failed to update link",
+//       });
+//     }
+//   });
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
---
+++ frontend/src/stores/index.js
@@ -0,0 +1,214 @@
+import { db, auth }  from "../config/firebase.js";
+import { signOut } from "firebase/auth";
+import { defineStore } from "pinia";
+import { collection, onSnapshot, } from "firebase/firestore";
+import Swal from "sweetalert2";
+import axios from "axios";
+
+const URL_API = "http://localhost:3000/";
+
+export const useApp = defineStore({
+  id: "App",
+  state: () => ({
+    input: {
+      links: {},
+      edit : {},
+  },
+    user: {
+      logged_in: false,
+    },
+    error: null,
+    linkys : [],
+  }),
+  actions: {
+    async login(email, password) {
+      console.log("login");
+      try {
+        await axios
+        .post(URL_API, {
+            email,
+            password,
+          })
+          .then((res) => {
+            document.cookie = `session=${res.data.result.session}; max-age=${res.data.result.options.maxAge};`;
+            this.router.push("/dashboard");
+          })
+          .catch((error) => {
+            console.log(error.response.data.message);
+            if (
+              error.response.data.message === "Firebase: Error (auth/user-not-found)."
+            ) {
+              let alert_1 = document.querySelector("#alert_1");
+              alert_1.classList.remove("d-none");
+              alert_1.innerHTML = "User not found !";
+            }
+            if (
+              error.response.data.message === "Firebase: Error (auth/wrong-password)."
+            ) {
+              let alert_1 = document.querySelector("#alert_1");
+              alert_1.classList.remove("d-none");
+              alert_1.innerHTML = "Wrong password !";
+            }
+          });
+      } catch (error) {
+        console.log(error);
+        this.error = error;
+      } 
+    },
+    async register(email, password, cpassword) {
+      this.error = null;
+      console.log("register");
+      try {
+        
+        await axios
+          .post(URL_API + "register", {
+            email,
+            password,
+            cpassword,
+          })
+          .then((res) => {
+            if(password !== cpassword) {
+              let alert_2 = document.querySelector("#alert_2");
+                    alert_2.classList.remove("d-none");
+                    alert_2.innerHTML = "Passwords don't match !";
+              return res.status(400).json({
+                  message: "Passwords don't match",
+              });
+          }
+            console.log(res);
+            this.router.push("/");
+          })
+          .catch((error) => {
+            console.log(error.response.data.message);
+            if (
+              error.response.data.message === "Firebase: Error (auth/email-already-in-use)."
+            ) {
+              let alert_2 = document.querySelector("#alert_2");
+              alert_2.classList.remove("d-none");
+              alert_2.innerHTML = "Email already in use !";
+            }
+          });
+      } catch (error) {
+        this.error = error;
+      } 
+    },
+    
+    async logout() {
+      await signOut(auth)
+
+     document.cookie = 'token=; Max-Age=0'
+      document.cookie = 'refreshToken=; Max-Age=0'
+      document.cookie = 'session=; Max-Age=0'
+
+      console.log("document.cookie", document.cookie)
+
+      this.router.push('/')
+    },
+
+    async sessionCheck() {
+
+      const session = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("session="))
+        ?.split("=")[1];
+
+      console.log("session : " + session);
+      console.log("document.cookie : ", document.cookie)
+
+ 
+      return false;
+    },
+    async shorten(links){
+      await axios
+      .post(URL_API + "dashboard", {
+        alias: links.alias,
+        url: links.url,
+        code: links.code,
+        editState: false,
+      })
+      .then((result) => {
+        console.log(result);
+        // Reset form
+        this.resetForm();
+        // Refresh link list
+        this.refreshList();
+      }).catch((error) => {
+        console.log(error);
+      });
+    },
+    async deleteLink(linky) {
+    
+          const id = linky.id 
+          console.log(linky.id)
+          await axios.delete(URL_API + "linky/" + id)
+  
+          
+          .then((result) => {
+            console.log(result);
+            // Reset form
+            this.resetForm();
+            // Refresh link list
+            this.refreshList();
+          }).catch((error) => {
+            console.log(error);
+          });
+   
+    },
+
+//     async editLink(linky, edit) {
+//       const id = linky.id 
+//           console.log(linky.id)
+//           await axios.post(URL_API + "linky/" + id, {
+//         alias: edit.alias,
+//         url: edit.url,
+//         code: edit.code,
+//         editState: false,
+//       })
+//       .then((result) => {
+//         console.log(result);
+//         // Reset form
+//         this.resetForm();
+//         // Refresh link list
+//         this.refreshList();
+//       }).catch((error) => {
+//         console.log(error);
+//       });
+
+// },
+    resetForm() {
+      this.input.links.url = '';
+      this.input.links.alias = '';
+      this.input.links.code = '';
+      this.input.edit.url = '';
+      this.input.edit.alias = '';
+      this.input.edit.code = '';
+    },
+    redirect(){
+      db.collection('urls').where('code', '==', this.$route.params.code).onSnapshot((linky) => {
+      if(linky.empty){
+        this.$router.push({name: '404'});
+      } else {
+        window.location.replace(linky.docs[0].data().url);
+      }
+    })
+    },
+    async refreshList() {
+      onSnapshot(collection(db, "urls"), (querySnapshot) => {
+        this.linkys = [];
+        querySnapshot.forEach((doc) => {
+          this.linkys.push({ id: doc.id, ...doc.data() });
+        });
+      },
+      error => {
+        Swal.fire({
+          title: 'Error!',
+          text: `Seems like there is an error while connecting to Firestore<br>${error}`,
+          icon: 'error',
+          confirmButtonText: 'Cool'
+        });
+      }
+      );
+    },
+  },
+});
+
