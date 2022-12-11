e72a0304a9fb4cb5d04ac97f8be3a2e38d0d9185
wahyuandhikarizaldi <113814423+wahyuandhikarizaldi@users.noreply.github.com>
2022-12-11 16:41:51 +0700

    Delete config directory

--- config/firebase-service-account.config.js
+++
@@ -1,8 +0,0 @@
-import admin from "firebase-admin";
-import { firebase_admin_config } from "./firebase-admin-sdk.config.js";
-
-admin.initializeApp({
-  credential: admin.credential.cert(firebase_admin_config)
-});
-
-export default admin;
